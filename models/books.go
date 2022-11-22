fd1d03e76bf4ba473bcd94e54de6dfd1f33d15a9
shashank <shashank944533@gmail.com>
2022-11-22 13:13:47 +0530

    commited first

---
+++ models/books.go
@@ -0,0 +1,15 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type Book struct {
+	ID        uint    `gorm:"primary key;autoIncrement" json:"id"`
+	Author    *string `json:"author"`
+	Title     *string `json:"title"`
+	Publisher *string `json:"publisher"`
+}
+
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
+	return err
+}
